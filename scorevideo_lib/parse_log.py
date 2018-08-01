81ba9489e4bd2d3fe4f145ab906c21cb4881d3a6
cs <cs.temporary@icloud.com>
2018-08-01 23:42:24 +0000

    Create framework for Log class

---
+++ scorevideo_lib/parse_log.py
@@ -0,0 +1,29 @@
+class Log:
+
+    @staticmethod
+    def get_section_header(log_file):
+        pass
+
+    @staticmethod
+    def get_section_video_info(log_file):
+        pass
+
+    @staticmethod
+    def get_section_commands(log_file):
+        pass
+
+    @staticmethod
+    def get_section_raw(log_file):
+        pass
+
+    @staticmethod
+    def get_section_full(log_file):
+        pass
+
+    @staticmethod
+    def get_section_notes(log_file):
+        pass
+
+    @staticmethod
+    def get_section_marks(log_file):
+        pass
