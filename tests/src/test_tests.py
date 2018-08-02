38e44ef05aa256ae0a39fa91860ca6d863447524
cs <cs.temporary@icloud.com>
2018-08-02 04:06:33 +0000

    Limit testing and development to Python 3.7

--- tests/src/test_tests.py
+++ tests/src/test_tests.py
@@ -1,7 +1,7 @@
 TEST_RES = "tests/res"
 
 def test_file_read():
-    expected = ["scorevideo LOG\r\n", "File:  log.mat"]
+    expected = ["scorevideo LOG\n", "File:  log.mat"]
     with open(TEST_RES + "/file_read.txt", 'r') as f:
         actual = f.readlines()
     assert(expected == actual)
