edd9300eae0e978b7485f9370596886d2a82b25f
cs <cs.temporary@icloud.com>
2018-08-03 19:22:35 +0000

    Re-structure Log class to re-use common section-getting code

--- scorevideo_lib/parse_log.py
+++ scorevideo_lib/parse_log.py
@@ -14,11 +14,13 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-class Log:
+from scorevideo_lib.exceptions import FileFormatError
+
+class Log(object):
 
     @staticmethod
     def get_section_header(log_file):
-        """Get the header section of a log
+        """Get the header section of a log.
 
         Extract the top section (top two lines) of a log. This section includes
         a statement that the log was created by
@@ -41,7 +43,7 @@ class Log:
 
     @staticmethod
     def get_section_video_info(log_file):
-        """Get the video info section of a log
+        """Get the video info section of a log.
 
         Extract the video info section (headed by the line "VIDEO FILE SET" of a
         log. This section includes
@@ -58,18 +60,11 @@ class Log:
             each line a separate element in the list.
             Newlines or return carriages are stripped from the ends of lines.
         """
-        while log_file.readline().rstrip() != "VIDEO FILE SET":
-            pass
-        video_info = []
-        line = log_file.readline().rstrip()
-        while not line == "":
-            video_info.append(line)
-            line = log_file.readline().rstrip()
-        return video_info
+        return Log.get_section(log_file, "VIDEO FILE SET", [], "")
 
     @staticmethod
     def get_section_commands(log_file):
-        """Get the commands section of a log
+        """Get the commands section of a log.
 
         Extract the commands section (headed by the line "COMMAND SET AND
         SETTINGS") used in generating the log file.
@@ -89,7 +84,7 @@ class Log:
 
     @staticmethod
     def get_section_raw(log_file):
-        """Get the raw log section of a log
+        """Get the raw log section of a log.
 
         Extract the section of the log that contains the raw scoring log. This
         section contains the frame number and
@@ -109,7 +104,7 @@ class Log:
 
     @staticmethod
     def get_section_full(log_file):
-        """Get the full log section of a log
+        """Get the full log section of a log.
 
         Extract the section of the log that contains the full scoring log. This
         section contains the frame number and
@@ -129,7 +124,7 @@ class Log:
 
     @staticmethod
     def get_section_notes(log_file):
-        """Get the notes section of a log
+        """Get the notes section of a log.
 
         Extract the notes section of the log, which contains arbitrary notes
         specified by the researcher during scoring,
@@ -148,7 +143,7 @@ class Log:
 
     @staticmethod
     def get_section_marks(log_file):
-        """Get the marks section of a log
+        """Get the marks section of a log.
 
         Extract the marks section of the log, which stores the frame number and
         time at which the video starts and stops.
@@ -165,3 +160,54 @@ class Log:
             Newlines or return carriages are stripped from the ends of lines.
         """
         pass
+
+    @staticmethod
+    def get_section(log_file, start, header, end):
+        """Get an arbitrary section from a log file.
+
+        Extract an arbitrary section from a log file. The section is defined by
+        a line at its start and a line at its end, neither of which are
+        considered part of the section (not returned). A header section is also
+        specified, the lines of which will be checked and excluded from the
+        section. A header starts on the line immediately following the start
+        line. If the header is not found, or if a line in it does not match, a
+        FileFormatError is raised. If the end of the file is unexpectedly found
+        before completing a section, a FileFormatError is raised.
+
+        Args:
+            log_file: An open file object that points to the log file to read.
+                The file object must be ready to be read,
+                and it should be at the start of the file.
+            start: Line that signals the start of the section
+            header: List of lines that form a header to the section. If no
+                header should be present, pass an empty list.
+            end: Line that signals the end of the section
+
+        Returns:
+            A list of the lines making up the section in sequential order, with
+            each line a separate element in the list.
+            Newlines or return carriages are stripped from the ends of lines.
+        """
+        line = log_file.readline()
+        while line.rstrip() != start:
+            if line == "":
+                raise FileFormatError("The start line '" + start +
+                                      "' was not found in " + log_file.name)
+            else:
+                line = log_file.readline()
+        for header_line in header:
+            found_line = log_file.readline().rstrip()
+            if header_line != found_line:
+                raise FileFormatError.from_lines(log_file.name, found_line,
+                                                 header_line)
+        section = []
+        line = log_file.readline()
+        while not line.rstrip() == end:
+            if line == "":
+                raise FileFormatError("The end line '" + end +
+                                      "' was not found in " + log_file.name)
+            line = line.rstrip()
+            section.append(line)
+            line = log_file.readline().rstrip()
+
+        return section
