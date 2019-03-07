991481ee84e5a000ad682e0face4c6bdd786f209
cs <cs.temporary@icloud.com>
2019-03-07 00:57:07 +0000

    Rearrange docstring on base_utils to be at top

--- scorevideo_lib/base_utils.py
+++ scorevideo_lib/base_utils.py
@@ -14,11 +14,11 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from typing import Callable, Iterable, List, Any
-
 """Basic utilities for generally applicable functions
 
 """
+
+from typing import Callable, Iterable, List
 
 
 class BaseOps:
